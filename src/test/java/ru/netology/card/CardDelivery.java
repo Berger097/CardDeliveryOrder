7e9a0b1a6e6d7cd7d74868c53c12038ce4a7f62e
Anatoly Moskalev <moskalev1997tolya@gmail.com>
2022-03-28 16:49:02 +0600

    Test

--- src/test/java/ru/netology/card/CardDelivery.java
+++ src/test/java/ru/netology/card/CardDelivery.java
@@ -1,34 +1,143 @@
 package ru.netology.card;
 
-import io.github.bonigarcia.wdm.WebDriverManager;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.commands.Val;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+
+import java.time.Duration;
+
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selectors.*;
+import static com.codeborne.selenide.Selenide.*;
 
 public class CardDelivery {
-    WebDriver driver;
-    @BeforeEach
-   public void setUp() {
-        WebDriverManager.chromedriver().setup();
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("--disable-dev-shm-usage");
-        options.addArguments("--no-sandbox");
-        options.addArguments("--headless");
-        driver = new ChromeDriver();
+    @Test
+   public void happyPath() {
+        Configuration.holdBrowserOpen = true;
+        open("http://0.0.0.0:9999/");
+        $("[data-test-id='city'] input").val("Омск");
+        $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
+        $("[data-test-id='date'] input").val("12.04.2022");
+        $("[data-test-id='name'] input").val("Елена");
+        $("[data-test-id='phone'] input").val("+79994702121");
+        $("[data-test-id='agreement']").click();
+        $("div .button").click();
+        $("[data-test-id='notification'] .notification__title").should(visible, Duration.ofSeconds(15));
 
-    }
-    @AfterEach
-    public void tearDown() {
-        driver.quit();
-        driver = null;
+
+
     }
     @Test
-    void test() {
+    public void invalidFieldCity() {
+        Configuration.holdBrowserOpen = true;
+        open("http://0.0.0.0:9999/");
+        $("[data-test-id='city'] input").val("Berlin");
+        $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
+        $("[data-test-id='date'] input").val("12.04.2022");
+        $("[data-test-id='name'] input").val("Елена");
+        $("[data-test-id='phone'] input").val("+79994702121");
+        $("[data-test-id='agreement']").click();
+        $("div .button").click();
+        $("[data-test-id='city'] .input__sub").should(visible, Duration.ofSeconds(15));
+
+
 
     }
+    @Test
+    public void emptyFieldCity() {
+        Configuration.holdBrowserOpen = true;
+        open("http://0.0.0.0:9999/");
+        $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
+        $("[data-test-id='date'] input").val("12.04.2022");
+        $("[data-test-id='name'] input").val("Елена");
+        $("[data-test-id='phone'] input").val("+79994702121");
+        $("[data-test-id='agreement']").click();
+        $("div .button").click();
+        $("[data-test-id='city'] .input__sub").should(visible, Duration.ofSeconds(15));
+
+
+
+    }
+    @Test
+    public void wrongDate() {
+        Configuration.holdBrowserOpen = true;
+        open("http://0.0.0.0:9999/");
+        $("[data-test-id='city'] input").val("Омск");
+        $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
+        $("[data-test-id='name'] input").val("Елена");
+        $("[data-test-id='phone'] input").val("+79994702121");
+        $("[data-test-id='agreement']").click();
+        $("div .button").click();
+        $("[data-test-id='date'] .input__sub").should(visible, Duration.ofSeconds(15));
+
+
+
+    }
+    @Test
+    public void invalidName() {
+        Configuration.holdBrowserOpen = true;
+        open("http://0.0.0.0:9999/");
+        $("[data-test-id='city'] input").val("Омск");
+        $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
+        $("[data-test-id='date'] input").val("12.04.2022");
+        $("[data-test-id='name'] input").val("Elena");
+        $("[data-test-id='phone'] input").val("+79994702121");
+        $("[data-test-id='agreement']").click();
+        $("div .button").click();
+        $("[data-test-id='name'] .input__sub").should(visible, Duration.ofSeconds(15));
+
+
+
+    }
+    @Test
+    public void noName() {
+        Configuration.holdBrowserOpen = true;
+        open("http://0.0.0.0:9999/");
+        $("[data-test-id='city'] input").val("Омск");
+        $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
+        $("[data-test-id='date'] input").val("12.04.2022");
+        $("[data-test-id='phone'] input").val("+79994702121");
+        $("[data-test-id='agreement']").click();
+        $("div .button").click();
+        $("[data-test-id='name'] .input__sub").should(visible, Duration.ofSeconds(15));
+
+
+
+    }
+    @Test
+    public void invalidPhone() {
+        Configuration.holdBrowserOpen = true;
+        open("http://0.0.0.0:9999/");
+        $("[data-test-id='city'] input").val("Омск");
+        $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
+        $("[data-test-id='date'] input").val("12.04.2022");
+        $("[data-test-id='name'] input").val("Елена");
+        $("[data-test-id='phone'] input").val("89994702121");
+        $("[data-test-id='agreement']").click();
+        $("div .button").click();
+        $("[data-test-id='phone'].input_invalid .input__sub").should(visible, Duration.ofSeconds(15));
+
+
+
+    }
+    @Test
+    public void noCheckBocks() {
+        Configuration.holdBrowserOpen = true;
+        open("http://0.0.0.0:9999/");
+        $("[data-test-id='city'] input").val("Омск");
+        $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
+        $("[data-test-id='date'] input").val("12.04.2022");
+        $("[data-test-id='name'] input").val("Елена");
+        $("[data-test-id='phone'] input").val("+79994702121");
+        $("div .button").click();
+        $("[data-test-id='agreement'].input_invalid .checkbox__text").should(visible, Duration.ofSeconds(15));
+
+
+
+    }
+
 
 
 }
